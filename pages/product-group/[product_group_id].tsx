b4df9f882e5035a21a816a327ff632f8a76b2c10
Rivo <rivo.rizaldy18@gmail.com>
2022-08-24 21:58:17 +0700

    layouting UI product group

---
+++ pages/product-group/[product_group_id].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductGroupDetail = () => {
+  return <div>ProductGroupDetail</div>;
+};
+
+export default ProductGroupDetail;
---
+++ pages/product-group/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductGroup = () => {
+  return <div>ProductGroup</div>;
+};
+
+export default ProductGroup;
