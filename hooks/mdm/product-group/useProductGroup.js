5362fba03dd12dc20af297f644fe8d0fb51ea5ef
Rivo <rivo.rizaldy18@gmail.com>
2022-08-24 21:58:17 +0700

    layouting UI product group

---
+++ hooks/mdm/product-group/useProductGroup.js
@@ -0,0 +1,106 @@
+import { useQuery, useMutation, useInfiniteQuery } from "react-query";
+import { mdmService } from "../../../lib/client";
+
+const fetchProductsGroup = async ({ query = {} }) => {
+  return mdmService(`/product-group`, {
+    params: {
+      search: "",
+      page: 1,
+      limit: 10,
+      sortBy: "created_at",
+      sortOrder: "DESC",
+      ...query,
+    },
+  }).then((data) => data);
+};
+
+const useProductsGroup = ({ query = {}, options }) => {
+  return useQuery(["products-group", query], () => fetchProductsGroup({ query }), {
+    ...options,
+  });
+};
+
+const fetchProductGroup = async ({ id, companyId }) => {
+  return mdmService(`/product-group/${companyId}/${id}`).then((data) => data);
+};
+
+const useProductGroup = ({ id, companyId, options }) => {
+  return useQuery(["product-group", id], () => fetchProductGroup({ id, companyId }), {
+    ...options,
+  });
+};
+
+function useFilterProductGroup({ options }) {
+  return useMutation(
+    (data) =>
+      mdmService(`/product-group/filter-product`, {
+        method: "POST",
+        data,
+      }),
+    {
+      ...options,
+    }
+  );
+}
+
+function useCreateProductGroup({ options }) {
+  return useMutation(
+    (data) =>
+      mdmService(`/product-group`, {
+        method: "POST",
+        data,
+      }),
+    {
+      ...options,
+    }
+  );
+}
+
+function useUpdateProductGroup({ id, companyId, options }) {
+  return useMutation(
+    (data) =>
+      mdmService(`/product-group/${companyId}/${id}`, {
+        method: "PUT",
+        data,
+      }),
+    {
+      ...options,
+    }
+  );
+}
+
+const useDeleteProductGroup = ({ options }) => {
+  return useMutation(
+    (data) =>
+      mdmService(`/product-group`, {
+        method: "DELETE",
+        data,
+      }),
+    {
+      ...options,
+    }
+  );
+};
+
+const useUploadFileProductGroup = ({ options }) => {
+  return useMutation(
+    (data) =>
+      mdmService(`/product-group/upload`, {
+        method: "POST",
+        data,
+      }),
+    {
+      ...options,
+    }
+  );
+};
+
+export {
+  useProductsGroup,
+  useProductGroup,
+  useFilterProductGroup,
+  useCreateProductGroup,
+  useUpdateProductGroup,
+  useDeleteProductGroup,
+  useUploadFileProductGroup,
+};
---
+++ pages/product-group/create.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductGroupCreate = () => {
+  return <div>ProductGroupCreate</div>;
+};
+
+export default ProductGroupCreate;
