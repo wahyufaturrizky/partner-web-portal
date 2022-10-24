9e4556fe673d3b168a04cc0275db5ab9b3a121e4
Rivo <rivo.rizaldy18@gmail.com>
2022-10-24 13:11:47 +0700

    fix menu design detail

--- pages/menu-config/design/index.tsx
+++ pages/menu-config/design/index.tsx
@@ -3,173 +3,169 @@ import Router from "next/router";
 import usePagination from "@lucasmogari/react-pagination";
 import { Button, Col, Pagination, Row, Search, Spacer, Table, Text } from "pink-lava-ui";
 
-import { colors } from "../../../utils/color";
-import { useDeleteMenuDesignList, useMenuDesignLists } from "../../../hooks/menu-config/useMenuDesign";
-import { ModalDeleteConfirmation } from "../../../components/elements/Modal/ModalConfirmationDelete";
+import { colors } from "utils/color";
+import { useDeleteMenuDesignList, useMenuDesignLists } from "hooks/menu-config/useMenuDesign";
+import { ModalDeleteConfirmation } from "components/elements/Modal/ModalConfirmationDelete";
 
 import styled from "styled-components";
 
 const MenuConfigDesign: any = () => {
-	const pagination = usePagination({
-		page: 1,
-		itemsPerPage: 20,
-		maxPageItems: Infinity,
-		numbers: true,
-		arrows: true,
-		totalItems: 100,
-	});
+  const pagination = usePagination({
+    page: 1,
+    itemsPerPage: 20,
+    maxPageItems: Infinity,
+    numbers: true,
+    arrows: true,
+    totalItems: 100,
+  });
 
-	const [search, setSearch] = useState("");
-	const [modalDelete, setModalDelete] = useState({ open: false });
+  const [search, setSearch] = useState("");
+  const [modalDelete, setModalDelete] = useState({ open: false });
 
-	const {
-		data: fields,
-		refetch: refetchFields,
-		isLoading: isLoadingField,
-	} = useMenuDesignLists({
-		options: {
-			onSuccess: (data: any) => {
-				pagination.setTotalItems(data.totalRow);
-			},
-		},
-		query: {
-			search,
-			page: pagination.page,
-			limit: pagination.itemsPerPage,
-		},
-	});
+  const {
+    data: fields,
+    refetch: refetchFields,
+    isLoading: isLoadingField,
+    isFetching: isFetchingField,
+  } = useMenuDesignLists({
+    query: {
+      search,
+      page: pagination.page,
+      limit: pagination.itemsPerPage,
+    },
+    options: {
+      onSuccess: (data: any) => {
+        pagination.setTotalItems(data.totalRow);
+      },
+      select: (data: any) => {
+        const mappedData = data?.rows?.map((element: any) => {
+          return {
+            key: element.id,
+            id: element.id,
+            field_name: element.name,
+            action: (
+              <div style={{ display: "flex", justifyContent: "left" }}>
+                <Button
+                  size="small"
+                  onClick={() =>
+                    Router.push({
+                      pathname: `/menu-config/design/${element.id}`,
+                    })
+                  }
+                  variant="tertiary"
+                >
+                  View Detail
+                </Button>
+              </div>
+            ),
+          };
+        });
 
-	const { mutate: deleteFields, isLoading: isLoadingDeleteProcessList } = useDeleteMenuDesignList({
-		options: {
-			onSuccess: () => {
-				refetchFields();
-				setModalDelete({ open: false });
-				setSelectedRowKeys([]);
-			},
-		},
-	});
+        return { data: mappedData, totalRow: data.totalRow };
+      },
+    },
+  });
 
-	const columns = [
-		{
-			title: "Menu Design Name",
-			dataIndex: "field_name",
-			width: "80%"
-		},
-		{
-			title: "Action",
-			dataIndex: "action",
-			render: (items: any) => (
-				<Button
-						size="small"
-						onClick={() =>
-							Router.push({
-								pathname: "/menu-design/detail",
-								query: items,
-							})
-						}
-						variant="tertiary"
-					>
-						View Detail
-					</Button>
-			)
-		},
-	];
+  const { mutate: deleteFields, isLoading: isLoadingDeleteProcessList } = useDeleteMenuDesignList({
+    options: {
+      onSuccess: () => {
+        refetchFields();
+        setModalDelete({ open: false });
+        setSelectedRowKeys([]);
+      },
+    },
+  });
 
-	const data: any = [];
-	fields?.rows?.map((field: any) => {
-		data.push({
-			key: field.id,
-			field_name: field.name,
-			field_module: field?.module?.name
-		});
-	});
+  const columns = [
+    {
+      title: "Menu Design Name",
+      dataIndex: "field_name",
+      width: "80%",
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      width: "15%",
+      align: "left",
+    },
+  ];
 
-	const paginateField = data;
-	const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
 
-	const onSelectChange = (selectedRowKeys: any) => {
-		setSelectedRowKeys(selectedRowKeys);
-	};
+  const onSelectChange = (selectedRowKeys: any) => {
+    setSelectedRowKeys(selectedRowKeys);
+  };
 
-	const rowSelection = {
-		selectedRowKeys,
-		onChange: onSelectChange,
-	};
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
 
-	return (
-		<>
-			<Col>
-				<Text variant={"h4"}>Menu Design</Text>
-				<Spacer size={20} />
-				<Card>
-					<Row justifyContent="space-between">
-						<Search
-							width="380px"
-							nameIcon="SearchOutlined"
-							placeholder="Search Menu Design Name"
-							colorIcon={colors.grey.regular}
-							onChange={({target}: any) => setSearch(target.value)}
-						/>
-						<Row gap="16px">
-							<Button
-								size="big"
-								variant={"tertiary"}
-								onClick={() => setModalDelete({ open: true })}
-								disabled={rowSelection.selectedRowKeys?.length === 0}
-							>
-								Delete
-							</Button>
-							<Button
-								size="big"
-								variant={"primary"}
-								onClick={() => Router.push("/menu-config/design/create")}
-							>
-								Create
-							</Button>
-						</Row>
-					</Row>
-				</Card>
-				<Spacer size={10} />
-				<Card style={{ padding: "16px 20px" }}>
-					<Col gap="60px">
-						<Table
-							loading={isLoadingField}
-							columns={columns.filter(
-								(filtering) =>
-									filtering.dataIndex !== "id" &&
-									filtering.dataIndex !== "created_at" &&
-									filtering.dataIndex !== "modified_by" &&
-									filtering.dataIndex !== "modified_at" &&
-									filtering.dataIndex !== "deleted_by" &&
-									filtering.dataIndex !== "deleted_at" &&
-									filtering.dataIndex !== "created_by"
-							)}
-							data={paginateField}
-							rowSelection={rowSelection}
-						/>
-						<Pagination pagination={pagination} />
-					</Col>
-				</Card>
-			</Col>
+  return (
+    <>
+      <Col>
+        <Text variant={"h4"}>Menu Design</Text>
+        <Spacer size={20} />
+        <Card>
+          <Row justifyContent="space-between">
+            <Search
+              width="380px"
+              nameIcon="SearchOutlined"
+              placeholder="Search Menu Design Name"
+              colorIcon={colors.grey.regular}
+              onChange={({ target }: any) => setSearch(target.value)}
+            />
+            <Row gap="16px">
+              <Button
+                size="big"
+                variant={"tertiary"}
+                onClick={() => setModalDelete({ open: true })}
+                disabled={rowSelection.selectedRowKeys?.length === 0}
+              >
+                Delete
+              </Button>
+              <Button
+                size="big"
+                variant={"primary"}
+                onClick={() => Router.push("/menu-config/design/create")}
+              >
+                Create
+              </Button>
+            </Row>
+          </Row>
+        </Card>
+        <Spacer size={10} />
+        <Card style={{ padding: "16px 20px" }}>
+          <Col gap="60px">
+            <Table
+              loading={isLoadingField || isFetchingField}
+              columns={columns}
+              data={fields?.data}
+              rowSelection={rowSelection}
+            />
+            <Pagination pagination={pagination} />
+          </Col>
+        </Card>
+      </Col>
 
-			{modalDelete.open && (
-				<ModalDeleteConfirmation
-					totalSelected={selectedRowKeys?.length}
-					itemTitle={paginateField?.find((menu: any) => menu.key === selectedRowKeys[0])?.field_name}
-					visible={modalDelete.open}
-					isLoading={isLoadingDeleteProcessList}
-					onCancel={() => setModalDelete({ open: false })}
-					onOk={() => deleteFields({ ids: selectedRowKeys })}
-				/>
-			)}
-		</>
-	);
+      {modalDelete.open && (
+        <ModalDeleteConfirmation
+          totalSelected={selectedRowKeys?.length}
+          itemTitle={fields?.find((menu: any) => menu.key === selectedRowKeys[0])?.field_name}
+          visible={modalDelete.open}
+          isLoading={isLoadingDeleteProcessList}
+          onCancel={() => setModalDelete({ open: false })}
+          onOk={() => deleteFields({ ids: selectedRowKeys })}
+        />
+      )}
+    </>
+  );
 };
 
 const Card = styled.div`
-	background: #ffffff;
-	border-radius: 16px;
-	padding: 16px;
+  background: #ffffff;
+  border-radius: 16px;
+  padding: 16px;
 `;
 
 export default MenuConfigDesign;
