6b545ff6e38a82da20277f7253c5d84a00c2253f
Rivo <rivo.rizaldy18@gmail.com>
2022-10-25 17:48:30 +0700

    Add enhance endpoint page account group

--- components/elements/Modal/ModalDetailAccountGroup.tsx
+++ components/elements/Modal/ModalDetailAccountGroup.tsx
@@ -1,91 +1,130 @@
 import React from "react";
-import { Button, Spacer, Modal, Input, Dropdown } from "pink-lava-ui";
+import { Button, Spacer, Modal, Input, Dropdown, Spin } from "pink-lava-ui";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 
-import { useAccountGroupParent } from "../../../hooks/finance-config/useAccountGroup";
+import {
+  useAccountGroupParent,
+  useAccountGroupsDetail,
+} from "../../../hooks/finance-config/useAccountGroup";
+import styled from "styled-components";
 
 const schema = yup
-	.object({
-		groupName: yup.string().required("Name is Required"),
-	})
-	.required();
+  .object({
+    groupName: yup.string().required("Name is Required"),
+  })
+  .required();
 
 export const ModalDetailAccountGroup: any = ({
-	visible,
-	defaultValue,
-	onCancel,
-	onOk,
-}: {
-	visible: any;
-	defaultValue: any;
-	onCancel: any;
-	onOk: any;
-	error: any;
-}) => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		setValue,
-	} = useForm({
-		defaultValues: defaultValue,
-		resolver: yupResolver(schema),
-	});
+  visible,
+  defaultValue,
+  onCancel,
+  onOk,
+  id,
+}: any) => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-	const onSubmit = (data: any) => onOk({ groupName: data.groupName, parentId: data.parentId });
+  const onSubmit = (data: any) => onOk({ groupName: data.groupName, parentId: data.parentId });
 
-	const { data: accountGroupParent, isLoading } = useAccountGroupParent({
-		options: {},
-	});
+  const { data: accountGroupParent } = useAccountGroupParent({
+    options: {},
+  });
 
-	const parents = accountGroupParent
-		?.filter((account: any) => account.id !== defaultValue.id)
-		.map((account: any) => ({ id: account.id, value: account.groupName }));
+  const {
+    data: accountGroup,
+    isLoading: isLoadingAccountGroup,
+    isFetching: isFetchingAccountGroup,
+  } = useAccountGroupsDetail({
+    id,
+    query: {},
+    options: {
+      onSuccess: (data: any) => {
+        console.log(data);
+      },
+    },
+  });
 
-	return (
-		<Modal
-			visible={visible}
-			onCancel={onCancel}
-			title={"Account Group"}
-			footer={
-				<div
-					style={{
-						display: "flex",
-						marginBottom: "12px",
-						marginRight: "12px",
-						justifyContent: "flex-end",
-						gap: "12px",
-					}}
-				>
-					<Button onClick={handleSubmit(onSubmit)} variant="primary" size="big">
-						Save
-					</Button>
-				</div>
-			}
-			content={
-				<>
-					<Spacer size={20} />
-					<Input
-						error={errors?.groupName?.message}
-						{...register("groupName", { required: true })}
-						label="Name"
-						placeholder="e.g Receivable"
-					/>
-					<Spacer size={20} />
-					<Dropdown
-						label="Parent (optional)"
-						placeholder="Select"
-						width="100%"
-						items={parents}
-						defaultValue={defaultValue?.parents?.id}
-						handleChange={(value: any) => setValue("parentId", value)}
-						noSearch
-					/>
-					<Spacer size={14} />
-				</>
-			}
-		/>
-	);
+  const parents = accountGroupParent
+    ?.filter((account: any) => account.id !== id)
+    .map((account: any) => ({ id: account.id, value: account.groupName }));
+
+  return (
+    <Modal
+      visible={visible}
+      onCancel={onCancel}
+      title={"Account Group"}
+      footer={
+        <div
+          style={{
+            display: "flex",
+            marginBottom: "12px",
+            marginRight: "12px",
+            justifyContent: "flex-end",
+            gap: "12px",
+          }}
+        >
+          <Button onClick={handleSubmit(onSubmit)} variant="primary" size="big">
+            Save
+          </Button>
+        </div>
+      }
+      content={
+        <>
+          {isLoadingAccountGroup || isFetchingAccountGroup ? (
+            <Center>
+              <Spin tip="Loading Data..." />
+            </Center>
+          ) : (
+            <>
+              <Spacer size={20} />
+              <Input
+                error={errors?.groupName?.message}
+                defaultValue={accountGroup?.groupName}
+                {...register("groupName", { required: true })}
+                label="Name"
+                placeholder="e.g Receivable"
+              />
+              <Spacer size={20} />
+
+              <Controller
+                control={control}
+                shouldUnregister={true}
+                defaultValue={accountGroup?.parents?.id}
+                name="parentId"
+                render={({ field: { onChange, value }, formState: { errors } }) => (
+                  <Dropdown
+                    label="Parent (optional)"
+                    placeholder="Select"
+                    width="100%"
+                    items={parents}
+                    defaultValue={value}
+                    handleChange={(value: any) => {
+                      onChange(value);
+                    }}
+                    noSearch
+                  />
+                )}
+              />
+
+              <Spacer size={14} />
+            </>
+          )}
+        </>
+      }
+    />
+  );
 };
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
