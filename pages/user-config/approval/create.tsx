06654c42650db8d42da6418ca6221d5d040801df
hari hari <harihari@haris-MacBook-Air.local>
2022-11-11 06:40:50 +0700

    approval list

--- pages/user-config/approval/create.tsx
+++ pages/user-config/approval/create.tsx
@@ -12,6 +12,10 @@ import {
 	Switch,
 	Table,
 	Text,
+	EmptyState,
+	Pagination,
+	FormSelect,
+	Spin
 } from "pink-lava-ui";
 import styled from "styled-components";
 
@@ -19,6 +23,9 @@ import { useConfigs } from "../../../hooks/config/useConfig";
 import { useCreatePartnerConfigApprovalList } from "../../../hooks/user-config/useApproval";
 import { usePermissions } from "../../../hooks/permission/usePermission";
 import { useProcessLists } from "../../../hooks/business-process/useProcess";
+import usePagination from "@lucasmogari/react-pagination";
+import { useUserInfiniteList } from "hooks/user-config/useUser";
+import useDebounce from "lib/useDebounce";
 
 export interface ConfigModuleList {}
 
@@ -33,10 +40,35 @@ const CreatePartnerConfigApproval: any = () => {
 		numberOfApprovalStage: 1,
 	});
 	const { name, numberOfApprovalStage } = stateFieldInput;
+	const [errors, setErrors] = useState({
+		name: '',
+		permission: '',
+		numberOfApprovalStage: ''
+	})
+	const [searchTop, setSearchTop] = useState("");
+  const debounceSearchTop = useDebounce(searchTop, 1000);
+	const [listTop, setListTop] = useState<any[]>([]);
+  const [totalRowsTop, setTotalRowsTop] = useState(0);
+
+	const pagination = usePagination({
+		page: 1,
+		itemsPerPage: 10,
+		maxPageItems: Infinity,
+		numbers: true,
+		arrows: true,
+		totalItems: numberOfApprovalStage,
+	});
+
 
 	const handleChangeInput = (e) => {
 		if (e.target.id === "numberOfApprovalStage") {
-			if (e.target.value > 0) {
+			if(e.target.value > 50){
+				setErrors(prev => ({ ...prev, numberOfApprovalStage: `The number of approval stages can't be more than 50 stages`}))
+				setStateFieldInput({
+					...stateFieldInput,
+					[e.target.id]: 50,
+				});
+			} else {
 				setStateFieldInput({
 					...stateFieldInput,
 					[e.target.id]: e.target.value,
@@ -57,17 +89,9 @@ const CreatePartnerConfigApproval: any = () => {
 		options: {
 			onSuccess: (data: any) => {
 				if (data) {
-					window.alert("Process created successfully");
 					Router.back();
 				}
-			},
-			onError: (error: any) => {
-				if (error?.data) {
-					window.alert(error.data.errors && error.data.errors[0].message);
-				} else {
-					window.alert(error.data.message);
-				}
-			},
+			}
 		},
 	});
 
@@ -89,26 +113,67 @@ const CreatePartnerConfigApproval: any = () => {
 		},
 	});
 
-	const handleCreateProcessList = () => {
-		const isEmptyField = Object.keys(stateFieldInput).find(
-			(thereIsEmptyField) => stateFieldInput && stateFieldInput[thereIsEmptyField] === ""
-		);
+	const {
+    isLoading: isLoadingTop,
+    isFetching: isFetchingTop,
+    isFetchingNextPage: isFetchingMoreTop,
+    hasNextPage: hasNextTop,
+    fetchNextPage: fetchNextPageTop,
+  } = useUserInfiniteList({
+    query: {
+      search: debounceSearchTop,
+      company_id: "KSNI",
+      limit: 10,
+    },
+    options: {
+      onSuccess: (data: any) => {
+        setTotalRowsTop(data?.pages[0].totalRow);
+        const mappedData = data?.pages?.map((group: any) => {
+          return group.rows?.map((element: any) => {
+            return {
+              label: element.name,
+              value: element.topId,
+            };
+          });
+        });
+        const flattenArray = [].concat(...mappedData);
+        setListTop(flattenArray);
+      },
+      getNextPageParam: (_lastPage: any, pages: any) => {
+        if (listTop.length < totalRowsTop) {
+          return pages.length + 1;
+        } else {
+          return undefined;
+        }
+      },
+    },
+  });
 
-		if (!isEmptyField) {
-			const data: any = {
-				name: name,
-				module_id: dataListDropdownModul,
-				process_id: dataListDropdownProcess,
-				partner_permission_id: dataListDropdownPermission,
-				approval_stages: approvalStages.map((dataApprovalStages) => ({
-					is_mandatory: dataApprovalStages.is_mandatory,
-				})),
-				is_email_notification: isSendEmailNotif,
-			};
-			mutateCreatePartnerConfigApprovalList(data);
-		} else {
-			window.alert(`field ${isEmptyField} must be fill!`);
+	const handleCreateProcessList = () => {
+		if(stateFieldInput['name'] === '' || !dataListDropdownPermission){
+			let errors:any = {};
+			if(stateFieldInput['name'] === ''){
+				errors.name = 'Field Name is Required'
+			}
+			if(!dataListDropdownPermission){
+				errors.permission = 'Field Permission is Required'
+			}
+
+			setErrors(errors);
+			return;
 		}
+
+		const data: any = {
+			name: name,
+			module_id: dataListDropdownModul,
+			process_id: dataListDropdownProcess,
+			partner_permission_id: dataListDropdownPermission,
+			approval_stages: approvalStages.map((dataApprovalStages) => ({
+				is_mandatory: dataApprovalStages.is_mandatory,
+			})),
+			is_email_notification: isSendEmailNotif,
+		};
+		mutateCreatePartnerConfigApprovalList(data);
 	};
 
 	const handleChangeDropdown = (value: any, name: any) => {
@@ -127,15 +192,15 @@ const CreatePartnerConfigApproval: any = () => {
 
 	const columnsAssociatedRoles = [
 		{
-			title: "Roles",
-			dataIndex: "roles_field",
+			title: "Stage",
+			dataIndex: "stage",
 		},
 		{
-			title: "Default Stage",
-			dataIndex: "default_stage_field",
+			title: "Role",
+			dataIndex: "role",
 			render: (value: any) => (
 				<Dropdown
-					width="100%"
+					width="200px"
 					loading={isLoadingConfigModule}
 					items={[{ id: 1, value: "stage 1" }]}
 					placeholder={"Select"}
@@ -144,23 +209,65 @@ const CreatePartnerConfigApproval: any = () => {
 			),
 		},
 		{
+			title: "User",
+			dataIndex: "user",
+			render: (value: any) => {
+				console.log('value 1', value)
+				return (
+					<>
+						{isLoadingTop ? (
+							<Center>
+								<Spin tip="" />
+							</Center>
+						) : (
+							<>
+								<FormSelect
+									defaultValue={value}
+									style={{ width: "100%", height: "48px" }}
+									size={"large"}
+									placeholder={"Select"}
+									borderColor={"#AAAAAA"}
+									arrowColor={"#000"}
+									withSearch
+									isLoading={isFetchingTop}
+									isLoadingMore={isFetchingMoreTop}
+									fetchMore={() => {
+										if (hasNextTop) {
+											fetchNextPageTop();
+										}
+									}}
+									items={isFetchingTop && !isFetchingMoreTop ? [] : listTop}
+									onChange={(value: any) => {
+										console.log('value 2', value)
+										//onChange(value?.toString());
+									}}
+									onSearch={(value: any) => {
+										setSearchTop(value);
+									}}
+								/>
+							</>
+						)}
+					</>
+				)
+			},
+		},
+		{
+			title: 'Cc Email',
+			dataIndex: 'cc_email',
+			align: 'center',
+			render: (value: any) => (
+				<Switch
+					defaultChecked={isSendEmailNotif}
+					checked={isSendEmailNotif}
+					onChange={() => setisSendEmailNotif(!isSendEmailNotif)}
+				/>
+			)
+		},
+		{
 			title: "Action",
 			dataIndex: "action",
-		},
-	];
-
-	const columnsAssociatedUsers = [
-		{
-			title: "Users",
-			dataIndex: "users_field",
-		},
-		{
-			title: "Roles",
-			dataIndex: "roles_field",
-		},
-		{
-			title: "Stages",
-			dataIndex: "stages_field",
+			width: "15%",
+      align: "left",
 		},
 	];
 
@@ -174,26 +281,54 @@ const CreatePartnerConfigApproval: any = () => {
 		setApprovalStages(tempApprovalStages);
 	}, [numberOfApprovalStage]);
 
+
+	const dataTable = approvalStages.map((data:any, index) => ({
+		key: 1,
+		stage: `Stage ${data.id + 1}`,
+		action: (
+			<div style={{ display: "flex", justifyContent: "left" }}>
+				<Button size="small" onClick={() => {}} variant="tertiary">
+					View Detail
+				</Button>
+			</div>
+		),
+	}))
+
+	const page = pagination?.page;
+	const paginateTableData = dataTable?.slice(10 * (page - 1), 10 * page) || [];
+
 	return (
 		<>
 			<Col>
 				<Row gap="4px">
 					<Text variant="h4">Create Approval</Text>
 				</Row>
+				<Spacer size={20} />
 				<Card>
 					<Row justifyContent="space-between" alignItems="center" nowrap>
-						<Row alignItems="center" gap="4px">
-							<Switch
-								defaultChecked={isSendEmailNotif}
-								checked={isSendEmailNotif}
-								onChange={() => setisSendEmailNotif(!isSendEmailNotif)}
+						<Row alignItems="center" gap="4px" noWrap>
+							<Row alignItems="center"	width="100%"  gap="4px" noWrap>
+								<Switch
+									defaultChecked={isSendEmailNotif}
+									checked={isSendEmailNotif}
+									onChange={() => setisSendEmailNotif(!isSendEmailNotif)}
+								/>
+								<div
+									style={{ cursor: "pointer" }}
+									onClick={() => setisSendEmailNotif(!isSendEmailNotif)}
+								>
+									<Text variant={"h6"}>Email Notification</Text>
+								</div>
+							</Row>
+							<Spacer size={8} />
+							<Input
+								id="reminder_day"
+								width="230px"
+								label=""
+								height="42px"
+								placeholder={"Type e.g 1 Reminder (Days)"}
+								onChange={handleChangeInput}
 							/>
-							<div
-								style={{ cursor: "pointer" }}
-								onClick={() => setisSendEmailNotif(!isSendEmailNotif)}
-							>
-								<Text variant={"h6"}>Email Notification</Text>
-							</div>
 						</Row>
 
 						<Row>
@@ -216,13 +351,6 @@ const CreatePartnerConfigApproval: any = () => {
 						<Accordion.Header variant="blue">General</Accordion.Header>
 						<Accordion.Body>
 							<Row width="100%" gap="20px" noWrap>
-								{/* TODO: HIDE AFTER INCLUDING IN SPRINT */}
-								{/* <Row alignItems="center" gap="4px">
-									<Checkbox checked={isApproval} onChange={() => setIsApproval(!isApproval)} />
-									<div style={{ cursor: "pointer" }} onClick={() => setIsApproval(!isApproval)}>
-										<Text variant={"h6"}>Approval</Text>
-									</div>
-								</Row> */}
 							</Row>
 							<Row width="100%" gap="20px" noWrap>
 								<Col width="100%">
@@ -233,6 +361,8 @@ const CreatePartnerConfigApproval: any = () => {
 										height="48px"
 										placeholder={"e.g Shipment and Delivery"}
 										onChange={handleChangeInput}
+										error={errors?.name}
+										required
 									/>
 								</Col>
 								<Col width="100%">
@@ -249,22 +379,18 @@ const CreatePartnerConfigApproval: any = () => {
 										handleChange={(value) => handleChangeDropdown(value, "module")}
 										noSearch
 									/>
-
+									<Spacer size={9} />
 									<Text
-										onClick={() => handleDirectAssociated("config")}
+										onClick={() => handleDirectAssociated("module-config")}
 										clickable
 										variant="headingSmall"
 										color="pink.regular"
 									>
 										Go to Associated Module
 									</Text>
-									{/* <div style={{ cursor: "pointer" }} onClick={() => {}}>
-										<Text variant="headingSmall" color="pink.regular">
-											Go to Associated Modul >
-										</Text>
-									</div> */}
 								</Col>
 							</Row>
+							<Spacer size={13} />
 							<Row width="100%" gap="20px" noWrap>
 								<Col width="100%">
 									<Dropdown
@@ -279,9 +405,9 @@ const CreatePartnerConfigApproval: any = () => {
 										handleChange={(value) => handleChangeDropdown(value, "process")}
 										noSearch
 									/>
-
+									<Spacer size={9} />
 									<Text
-										onClick={() => handleDirectAssociated("process")}
+										onClick={() => handleDirectAssociated("business-process/process")}
 										variant="headingSmall"
 										color="pink.regular"
 										clickable
@@ -289,6 +415,7 @@ const CreatePartnerConfigApproval: any = () => {
 										Go to Associated Process
 									</Text>
 								</Col>
+
 								<Col width="100%">
 									<Dropdown
 										width="100%"
@@ -301,21 +428,18 @@ const CreatePartnerConfigApproval: any = () => {
 										placeholder={"Select"}
 										handleChange={(value) => handleChangeDropdown(value, "permission")}
 										noSearch
+										error={errors.permission}
+										required
 									/>
-
+									<Spacer size={9} />
 									<Text
-										onClick={() => handleDirectAssociated("permission")}
+										onClick={() => handleDirectAssociated("user-config/permission")}
 										variant="headingSmall"
 										color="pink.regular"
 										clickable
 									>
 										Go to Associated Permission
 									</Text>
-									{/* <div style={{ cursor: "pointer" }} onClick={() => {}}>
-										<Text variant="headingSmall" color="pink.regular">
-											Go to Associated Modul >
-										</Text>
-									</div> */}
 								</Col>
 							</Row>
 						</Accordion.Body>
@@ -329,13 +453,6 @@ const CreatePartnerConfigApproval: any = () => {
 						<Accordion.Header variant="blue">Approval</Accordion.Header>
 						<Accordion.Body>
 							<Row width="100%" gap="20px" noWrap>
-								{/* TODO: HIDE AFTER INCLUDING IN SPRINT */}
-								{/* <Row alignItems="center" gap="4px">
-									<Checkbox checked={isApproval} onChange={() => setIsApproval(!isApproval)} />
-									<div style={{ cursor: "pointer" }} onClick={() => setIsApproval(!isApproval)}>
-										<Text variant={"h6"}>Approval</Text>
-									</div>
-								</Row> */}
 							</Row>
 							<Row width="100%" gap="20px" noWrap>
 								<Col width="50%">
@@ -348,15 +465,19 @@ const CreatePartnerConfigApproval: any = () => {
 										placeholder={"e.g 1"}
 										onChange={handleChangeInput}
 										type="number"
+										error={errors.numberOfApprovalStage}
+										onFocus={
+											() => setErrors(prev => ({ ...prev, numberOfApprovalStage: ``}))
+										}
 									/>
-
+									<Spacer size={9} />
 									<Text clickable variant="headingSmall">
 										Choose mandatory approval stages
 									</Text>
 
 									<Spacer size={24} />
 
-									<Row gap="24px">
+									<Grid gap="24px" width="100%">
 										{approvalStages.map((data, index) => (
 											<Col key={index}>
 												<Row alignItems="center">
@@ -393,7 +514,8 @@ const CreatePartnerConfigApproval: any = () => {
 												</Row>
 											</Col>
 										))}
-									</Row>
+										<Spacer size={5} />
+									</Grid>
 								</Col>
 							</Row>
 						</Accordion.Body>
@@ -406,44 +528,28 @@ const CreatePartnerConfigApproval: any = () => {
 					<Accordion.Item key={1}>
 						<Accordion.Header variant="blue">Associated Roles</Accordion.Header>
 						<Accordion.Body>
-							<Table
-								data={[
-									{
-										key: 1,
-										roles_field: "",
-										default_stage_field: "",
-										action: (
-											<div style={{ display: "flex", justifyContent: "left" }}>
-												<Button size="small" onClick={() => {}} variant="tertiary">
-													View Detail
-												</Button>
-											</div>
-										),
-									},
-								]}
-								columns={columnsAssociatedRoles}
-							/>
-						</Accordion.Body>
-					</Accordion.Item>
-				</Accordion>
-
-				<Spacer size={20} />
-
-				<Accordion>
-					<Accordion.Item key={1}>
-						<Accordion.Header variant="blue">Associated Users</Accordion.Header>
-						<Accordion.Body>
-							<Table
-								data={[
-									{
-										key: 1,
-										users_field: "",
-										roles_field: "",
-										stage_field: "",
-									},
-								]}
-								columns={columnsAssociatedUsers}
-							/>
+							{approvalStages.length === 0 ? (
+								<Col>
+									<Text variant="body1" color="cheese.darker">*Auto added from permission Approval Payment is in the Role below</Text>
+									<Spacer size={20} />
+									<EmptyState
+										image={"/icons/empty-state.svg"}
+										title={"There's no associated role & user yet"}
+										subtitle={`Please add permission to a user/role`}
+										height={214}
+									/>
+								</Col>
+							) : (
+								<Col>
+									<Text variant="body1" color="cheese.darker">*Auto added from permission Approval Payment is in the Role below</Text>
+									<Spacer size={20} />
+									<Table
+										data={paginateTableData}
+										columns={columnsAssociatedRoles}
+									/>
+									{pagination.totalItems > 5 && <Pagination pagination={pagination} />}
+								</Col>
+							)}
 						</Accordion.Body>
 					</Accordion.Item>
 				</Accordion>
@@ -452,6 +558,12 @@ const CreatePartnerConfigApproval: any = () => {
 	);
 };
 
+const Grid = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: repeat(10, 100px);
+	gap: 40px 10px;
+`
 const Span = styled.div`
 	font-size: 14px;
 	line-height: 18px;
@@ -471,6 +583,12 @@ const Card = styled.div`
 	background: #ffffff;
 	border-radius: 16px;
 	padding: ${(p) => (p.padding ? p.padding : "16px")};
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default CreatePartnerConfigApproval;
