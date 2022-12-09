627de0d08ac267a7a910d4267e93375e8057c114
Geny Kurniawan T <geny.k@nabatisnack.co.id>
2022-12-09 19:42:14 +0700

    Add company id

--- hooks/finance-config/useAccountGroup.js
+++ hooks/finance-config/useAccountGroup.js
@@ -33,7 +33,8 @@ const useAccountGroupParent = ({ query = {}, options } = {}) => {
 };
 
 const fetchAccountGroupDetail = async ({ query = {}, id }) => {
-  return client(`/account-group/${id}`).then((data) => data);
+  const companyCode = localStorage.getItem('companyCode');
+  return client(`/account-group/${companyCode}/${id}`).then((data) => data);
 };
 
 const useAccountGroupsDetail = ({ query = {}, options, id } = {}) => {
