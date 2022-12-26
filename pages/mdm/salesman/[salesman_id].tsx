cc3de5c622ce897e11a093076f28419b26794e24
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2022-12-26 16:52:07 +0700

    fix: salesman detail customer adjust

--- pages/mdm/salesman/[salesman_id].tsx
+++ pages/mdm/salesman/[salesman_id].tsx
@@ -1,17 +1,5 @@
-import React from 'react';
-import ComponentDetailSalesman from '../../../components/pages/Salesman/DetailSalesman';
-import { useListCustomers } from '../../../hooks/mdm/customers/useCustomersMDM';
+import ComponentDetailSalesman from "../../../components/pages/Salesman/DetailSalesman";
 
 export default function DetailSalesman() {
-  const { data: listCustomers, isLoading } = useListCustomers({
-    options: { onSuccess: () => {} },
-    query: {},
-  });
-
-  return (
-    <ComponentDetailSalesman
-      isLoading={isLoading}
-      listCustomers={listCustomers?.rows}
-    />
-  );
+  return <ComponentDetailSalesman />;
 }
